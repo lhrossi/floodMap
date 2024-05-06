19a957ee23d943bf6c4690817bd1606043c41ce7
Rafael Silva <rafael.silva@e-hps.com>
2024-05-06 15:52:20 -0300

    refatoração dos filtros

--- components/Necessidades.vue
+++ components/Necessidades.vue
@@ -2,9 +2,7 @@
   <div v-if="necessidades.length > 0" class="flex flex-column gap-1">
     <v-divider class="my-2" />
     <h5 class="font-bold">Necessidades:</h5>
-    <span v-for="necessidade in necessidades" :class="necessidade.class">{{
-      necessidade.nome
-    }}</span>
+    <span v-for="necessidade in necessidades" :class="necessidade.class">{{ necessidade.nome }}</span>
   </div>
 </template>
 
@@ -14,14 +12,11 @@ const props = defineProps<{ abrigo: any }>();
 const necessidades = computed(() => {
   const necessidades = [] as { nome: string; class?: string }[];
 
-  if (props.abrigo.estrutura_pessoas)
-    necessidades.push({ nome: "Voluntários" });
+  if (props.abrigo.estrutura_pessoas) necessidades.push({ nome: "Voluntários" });
   if (props.abrigo.roupa_cama) necessidades.push({ nome: "Roupas de Cama" });
   if (props.abrigo.cozinha) necessidades.push({ nome: "Alimentos" });
-  if (props.abrigo.banheiros)
-    necessidades.push({ nome: "Produtos de Higiene" });
-  if (props.abrigo.demanda)
-    necessidades.push({ nome: props.abrigo.demanda, class: "text-warning" });
+  if (props.abrigo.banheiros) necessidades.push({ nome: "Produtos de Higiene" });
+  if (props.abrigo.demanda) necessidades.push({ nome: props.abrigo.demanda, class: "text-warning" });
 
   return necessidades;
 });
