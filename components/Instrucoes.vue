b793f7bb3b1bcd767b1faa44c2227496dede9bcb
ljsantos <lucas.santos@huia.haus>
2024-05-09 05:39:59 -0300

    impr: refactor modal component structure and styles

--- components/Instrucoes.vue
+++ components/Instrucoes.vue
@@ -1,8 +1,6 @@
 <template>
-  <v-container class="instructions-modal flex flex-col">
-    <div class="title">
-      <h3>Aprenda como utilizar o mapa</h3>
-    </div>
+  <div class="text">
+    <h3>Aprenda como utilizar o mapa</h3>
     <v-container class="steps mb-5">
       <div class="step">
         <div>
@@ -34,100 +32,66 @@
         </div>
       </div>
     </v-container>
-    <PrimaryButton fullWidth color="primary" rounded="xl" size="x-large" text="Voltar" :click="() => props.closeModal()" />
-  </v-container>
+  </div>
+
 </template>
 
 <script setup>
-const props = defineProps({
-  closeModal: Function,
-})
 </script>
 
-<style scoped lang="scss">
-.instructions-modal {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background-color: white;
-  padding: 20px;
-  border-radius: 10px 10px 0 0;
-  max-height: 90vh;
-  z-index: 5;
-  max-width: 500px;
+<style lang="scss">
+h3 {
+  font-size: 1.5rem;
+  font-weight: 600;
+  line-height: 2rem;
+  letter-spacing: 0.0025em;
+  text-align: left;
+}
 
-  h3 {
-    font-size: 1.5rem;
-    font-weight: 600;
-    line-height: 2rem;
-    letter-spacing: 0.0025em;
-    text-align: left;
-  }
+.steps {
+  padding: 0;
+  .step {
+    margin-bottom: 20px;
 
-  .steps {
-    padding: 0;
-    max-height: 90%;
-    overflow-y: auto;
+    &-title {
+      display: flex;
+      align-items: center;
+      margin-bottom: 10px;
 
-    &::-webkit-scrollbar {
-      width: 8px;
-      margin: 10px;
-    }
-
-    &::-webkit-scrollbar-track {
-      background-color: #F5F5F5;
-      width: 8px;
-    }
-
-    &::-webkit-scrollbar-thumb {
-      background-color: #B2B2B2;
-      border-radius: 4px;
-      width: 8px;
-    }
-
-    .step {
-      margin-bottom: 20px;
-
-      &-title {
+      .tag {
+        background-color: #114DE6;
+        color: white;
+        border-radius: 50px;
         display: flex;
+        justify-content: center;
         align-items: center;
-        margin-bottom: 10px;
-
-        .tag {
-          background-color: #114DE6;
-          color: white;
-          border-radius: 50px;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          margin-right: 10px;
-        }
-
-        h4 {
-          font-size: 20px;
-          font-weight: 600;
-          line-height: 28px;
-          letter-spacing: 0.0025em;
-          text-align: left;
-
-        }
-
-        .content p {
-          font-size: 0.875rem;
-          font-weight: 400;
-          line-height: 1.5rem;
-          letter-spacing: 0.005em;
-          text-align: left;
-
-        }
+        margin-right: 10px;
       }
 
-      img {
-        width: 100%;
-        height: auto;
+      h4 {
+        font-size: 20px;
+        font-weight: 600;
+        line-height: 28px;
+        letter-spacing: 0.0025em;
+        text-align: left;
+
+      }
+
+      .content p {
+        font-size: 0.875rem;
+        font-weight: 400;
+        line-height: 1.5rem;
+        letter-spacing: 0.005em;
+        text-align: left;
+
       }
     }
+
+    img {
+      width: 100%;
+      height: auto;
+    }
+
   }
 }
 </style>
