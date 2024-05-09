3247e3b59a55d4578bd5f8eb5ce19a15bec8a392
Gabriel-025 <gabrielmachado210prata@gmail.com>
2024-05-09 20:49:21 -0300

    impr:header e parte do filtro de abrigos

--- components/Filtros.vue
+++ components/Filtros.vue
@@ -1,19 +1,55 @@
 <template>
-  <v-navigation-drawer temporary>
+  <v-navigation-drawer temporary class="drawer-style">
     <div class="flex flex-column filtros">
-      <span>Filtrar abrigos</span>
-      <v-select :label="'Cidades'" :items="cidades" v-model="cidade" />
-      <v-chip
-        v-for="filtro of filtrosPreDefinidos"
-        v-on:click="filtro.habilitado = !filtro.habilitado"
-        :variant="filtro.habilitado ? 'elevated' : 'outlined'"
-        size="small"
-        color="primary"
-        :key="filtro.nome"
+      <div
+        class="px-4 py-4 border-b flex justify-between items-center header-filter"
       >
-        {{ filtro.nome }}: {{ abrigosPorCidade?.filter(filtro.filtro).length }}
-      </v-chip>
-      <v-btn size="small">Fechar</v-btn>
+        <span class="text-2xl">Encontrar abrigo | Filtros</span>
+        <v-btn
+          elevation="0"
+          rounded="xl"
+          size="x-small"
+          class="flex items-center justify-center"
+          >&#x2715;
+        </v-btn>
+      </div>
+      <span class="px-4 flex flex-col gap-1 pt-8">
+        <v-select
+          :label="'Cidades'"
+          :items="cidades"
+          v-model="cidade"
+          class="w-full"
+        />
+        <v-chip
+          v-for="filtro of filtrosPreDefinidos"
+          v-on:click="filtro.habilitado = !filtro.habilitado"
+          :variant="filtro.habilitado ? 'elevated' : 'outlined'"
+          size="small"
+          color="primary"
+          :key="filtro.nome"
+        >
+          {{ filtro.nome }}:
+          {{ abrigosPorCidade?.filter(filtro.filtro).length }}
+        </v-chip>
+      </span>
+      <div class="px-4 flex gap-2 w-full pt-12 border-t mt-12">
+        <v-btn
+          elevation="0"
+          rounded="xl"
+          class="flex-1"
+          size="large"
+          border="#CBCBCB sm"
+          >Limpar filtro</v-btn
+        >
+        <v-btn
+          elevation="0"
+          rounded="xl"
+          class="flex-1"
+          size="large"
+          color="primary"
+          >Aplicar
+        </v-btn>
+      </div>
     </div>
   </v-navigation-drawer>
 </template>
@@ -31,31 +67,106 @@ const cidades = computed(() => {
       .map((item) => item.city)
       .filter((city, index, self) => self.indexOf(city) === index)
       .filter((city) => city && city != "")
-  )
-})
+  );
+});
 
 const filtrosPreDefinidos = ref([
-  { nome: "Com vagas", habilitado: false, filtro: (abrigo: any) => parseInt(abrigo.vagas_ocupadas) < parseInt(abrigo.vagas) },
-  { nome: "Com Cozinha", habilitado: false, filtro: (abrigo: any) => abrigo.cozinha == true },
-  { nome: "Com Banheiros", habilitado: false, filtro: (abrigo: any) => abrigo.banheiros == true },
-  { nome: "Com Demanda", habilitado: false, filtro: (abrigo: any) => abrigo.demanda != null },
+  {
+    nome: "Com vagas",
+    habilitado: false,
+    filtro: (abrigo: any) =>
+      parseInt(abrigo.vagas_ocupadas) < parseInt(abrigo.vagas),
+  },
+  {
+    nome: "Com Cozinha",
+    habilitado: false,
+    filtro: (abrigo: any) => abrigo.cozinha == true,
+  },
+  {
+    nome: "Com Banheiros",
+    habilitado: false,
+    filtro: (abrigo: any) => abrigo.banheiros == true,
+  },
+  {
+    nome: "Com Demanda",
+    habilitado: false,
+    filtro: (abrigo: any) => abrigo.demanda != null,
+  },
 ]);
 
 const abrigosPorCidade = computed(() => {
   if (!props.abrigos) return [];
 
-  return props.abrigos.filter((abrigo) => cidade.value == "Todos" || abrigo.city == cidade.value);
+  return props.abrigos.filter(
+    (abrigo) => cidade.value == "Todos" || abrigo.city == cidade.value
+  );
 });
 
 const filtrarDados = () => {
   if (!props.abrigos) return [];
 
-  const filtrosHabilitados = filtrosPreDefinidos.value.filter((filtro) => filtro.habilitado);
+  const filtrosHabilitados = filtrosPreDefinidos.value.filter(
+    (filtro) => filtro.habilitado
+  );
 
-  let abrigosFiltrados = abrigosPorCidade.value.filter((a) => filtrosHabilitados.length == 0 || filtrosHabilitados.some((f) => f.filtro(a)));
+  let abrigosFiltrados = abrigosPorCidade.value.filter(
+    (a) =>
+      filtrosHabilitados.length == 0 ||
+      filtrosHabilitados.some((f) => f.filtro(a))
+  );
 
   emit("filterChange", abrigosFiltrados);
 };
 
 watch([cidade, filtrosPreDefinidos], filtrarDados, { deep: true });
 </script>
+
+<style lang="scss">
+.v-navigation-drawer {
+  top: 0 !important;
+  min-height: 100dvh !important;
+  z-index: 1000 !important;
+  padding: 0;
+}
+
+.v-navigation-drawer--active {
+  min-width: 536px;
+}
+
+@media (max-width: 768px) {
+  .v-navigation-drawer--active {
+    min-width: 100%;
+  }
+}
+
+.v-navigation-drawer__scrim {
+  opacity: 0.5 !important;
+  z-index: 910 !important;
+}
+
+.drawer-style .v-field__overlay {
+  background-color: #ffffff;
+  border: 1.4px solid #000000;
+  border-radius: 8px;
+}
+
+.drawer-style .mdi-menu-down:before {
+  content: "⌄";
+  position: absolute;
+  height: 34px;
+  color: #1351b4;
+}
+
+.drawer-style .filtros {
+  padding: 0;
+}
+
+.drawer-style .header-filter .v-btn {
+  width: 36px;
+  height: 36px;
+  background: #ffffff;
+  border: 1px solid #cbcbcb;
+  font-size: 14px;
+  color: #1351b4;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -10,22 +10,39 @@
           </div>
           <div class="flex flex-auto"></div>
           <div class="flex align-center">
-            <v-btn color="dark" text :class="{ active: currentButton === 'mapa', 'menu-item': true }"
-              @click="() => selectButton('mapa')">
+            <v-btn
+              color="dark"
+              text
+              :class="{ active: currentButton === 'mapa', 'menu-item': true }"
+              @click="() => selectButton('mapa')"
+            >
               <span>Mapa</span>
               <div class="underline"></div>
             </v-btn>
-            <v-btn color="dark" text :class="{ active: currentButton === 'sobre', 'menu-item': true }"
-              @click="() => selectButton('sobre')">Sobre
+            <v-btn
+              color="dark"
+              text
+              :class="{ active: currentButton === 'sobre', 'menu-item': true }"
+              @click="() => selectButton('sobre')"
+              >Sobre
               <div class="underline"></div>
             </v-btn>
-            <v-btn color="dark" text href="https://abrigospoa.web.app/login" target="_blank">Login</v-btn>
+            <v-btn
+              color="dark"
+              text
+              href="https://abrigospoa.web.app/login"
+              target="_blank"
+              >Login</v-btn
+            >
           </div>
         </div>
       </v-toolbar-title>
     </v-app-bar>
     <v-main>
-      <Modal v-if="currentButton === 'sobre'" :click="() => selectButton('mapa')">
+      <Modal
+        v-if="currentButton === 'sobre'"
+        :click="() => selectButton('mapa')"
+      >
         <AboutModal />
       </Modal>
       <slot></slot>
@@ -34,12 +51,12 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
-const currentButton = ref('mapa')
+const currentButton = ref("mapa");
 const selectButton = (button) => {
-  currentButton.value = button
-}
+  currentButton.value = button;
+};
 </script>
 
 <style lang="scss">
@@ -49,20 +66,29 @@ const selectButton = (button) => {
   }
 }
 
+.v-toolbar {
+  border-radius: 0 0 16px 16px;
+  box-shadow: 0px 8px 24px rgba(0, 0, 0, 0.16);
+}
+
+.v-toolbar.v-toolbar--flat .v-toolbar__content {
+  height: 84px !important;
+}
+
 .v-btn__content {
   position: relative;
   display: flex;
 }
 
 .active {
-  color: #1351B4 !important;
+  color: #1351b4 !important;
 
   .underline {
     height: 3px;
     position: absolute;
     display: block;
     border-radius: 100px;
-    background-color: #4E9FFF;
+    background-color: #4e9fff;
     bottom: -6px;
     width: 100%;
     max-width: 100%;
