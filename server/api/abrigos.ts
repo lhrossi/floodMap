7336d6abb39f20a9b9dc29388e4a6d9902c7be06
Rafael Silva <rafael.silva@e-hps.com>
2024-05-09 22:52:34 -0300

    adicionar token no query string

--- server/api/abrigos.ts
+++ server/api/abrigos.ts
@@ -1,31 +1,35 @@
-import axios from 'axios'
+import axios from "axios";
 
 export default defineEventHandler(async (event) => {
-  const config = useRuntimeConfig()
+  const config = useRuntimeConfig();
 
   try {
+    let token = config.API_TOKEN as string;
+    if (event.path.includes("token"))
+      token = event.path.split("token=").pop()!.split("&")[0];
+
     const response = await axios.get(`${config.API_URL}/abrigos`, {
       headers: {
-        'Authorization': config.API_TOKEN
-      }
+        Authorization: token,
+      },
     });
-    
+
     const data = response.data.map((item: any) => {
       if (!item.longitude || !item.latitude) {
         // Replace missing latitude and longitude with default values
         return {
           ...item,
           latitude: -30,
-          longitude: -50
+          longitude: -50,
         };
       } else {
         return item;
       }
     });
 
-    return data
+    return data.slice(0, 10);
   } catch (err) {
-    console.error("err", err)
-    return []
+    console.error("err", err);
+    return [];
   }
-})
+});
