406a70e780eaf3cfb1e5bb785dbe8aafa86bbe40
Rodrigo Haertel <rodrigo.haertel@yahoo.com.br>
2024-05-11 20:15:03 -0300

    fix: map popup perfomance (#12)
    authored-by: Rodrigo Haertel <rodrigo.haertel@twism.com>

---
+++ components/Popup.vue
@@ -0,0 +1,28 @@
+<template>
+  <LazyMapboxDefaultPopup
+    :popup-id="`popup-${abrigo.id}`"
+    :lnglat="[abrigo.longitude, abrigo.latitude]"
+    :options="{ closeOnClick: true, closeButton: true }"
+  >
+    <h3 v-if="abrigo.nome">{{ abrigo.nome }}</h3>
+
+    <p v-if="abrigo.address">{{ abrigo.address }}</p>
+
+    <p v-if="abrigo.nome_contato || abrigo.telefone">
+      {{ abrigo.nome_contato }}
+      <span v-show="abrigo.telefone">- {{ abrigo.telefone }}</span>
+    </p>
+
+    <v-divider class="my-2" />
+
+    <ContagemVagas :abrigo="abrigo" />
+
+    <Necessidades :abrigo="abrigo" />
+
+    <ComoChegar :abrigo="abrigo" />
+  </LazyMapboxDefaultPopup>
+</template>
+
+<script setup lang="ts">
+defineProps<{ abrigo: any }>();
+</script>
