788b038c5e33dcff09bddcbca44358943227ce43
Leonardo Gruppelli <leonardo.gruppelli@melhorenvio.com>
2024-05-13 10:23:38 -0300

    feat: init floating bar updates

---
+++ components/CustomSelect.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="relative">
+    <div class="flex items-center p-2 relative bg-neutral-100 rounded-3xl font-bold text-neutral-600">
+      
+    </div>
+  </div>
+</template>
