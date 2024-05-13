7dfaa2f5c24c2480253f99877588fdd352daf3f9
Leonardo Gruppelli <leonardo.gruppelli@melhorenvio.com>
2024-05-13 10:23:38 -0300

    feat: init floating bar updates

--- utils/calcularCor.ts
+++ utils/calcularCor.ts
@@ -3,12 +3,18 @@ export default function calcularCor(vagas: any, vagasOcupadas: any) {
   if (isNaN(vagas) || isNaN(vagasOcupadas) || vagas <= 0) {
     return "#007972";
   }
+  
   const percentual = (vagasOcupadas * 100) / vagas;
+
   if (percentual < 0) {
     return "#007972";
   }
+
   if (percentual <= 25) return '#007972'
+  
   if (percentual <= 50) return '#114DE6'
+
   if (percentual <= 75) return '#E67711'
+
   return '#E61127'
 }
